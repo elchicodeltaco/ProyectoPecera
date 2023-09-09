9ed9de65dcda9a0687045d7c2e1200071d223525
AnthonyPriZep <144396660+AnthonyPriZep@users.noreply.github.com>
2023-09-09 01:11:09 -0600

    Add files via upload

---
+++ PeceraDeIA/Assets/Scripts/Scripst2/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public int vidas;
+
+    public int puntos;
+
+    public GameObject panelDeBotones;
+    public Text vidasText;
+    public Text PuntajeText;
+
+    public Text GameOverText;
+
+    private bool GameOver;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelDeBotones.SetActive(false);
+       // TextoFinPartida
+        vidasText.text = "Vidas: " + vidas;
+
+        GameOverText.gameObject.SetActive(false); 
+
+        Time.timeScale = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void quitarVida()
+    {
+        vidas--;
+
+        vidasText.text = "Vidas: " + vidas;
+
+        if (vidas <= 0)
+        {
+            GameOver = true;
+            Time.timeScale = 0f;
+            panelDeBotones.SetActive(true);
+        }
+
+    }
+
+    public void SumarPuntos()
+    {
+        puntos++;
+        PuntajeText.text = puntos.ToString();   
+    }
+
+
+   
+}
---
+++ PeceraDeIA/Assets/Scripts/Scripst2/SpawnerObjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerObjects : MonoBehaviour
+{
+    public GameObject prefabSpawner; //el object al que le haremos copias
+
+    public float tiempoMinimoSpawn;//intervalo para crear copias
+    public float tiempoMaxSpawn;
+
+    private float tiempoSeleccionado;//el tiempo escogido para generar otro objeto
+
+    //llevar la cuenta del spawn
+    private float ultimoMomentoSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempoSeleccionado = Random.Range(tiempoMinimoSpawn, tiempoMaxSpawn);
+
+        ultimoMomentoSpawn = 0f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= ultimoMomentoSpawn + tiempoSeleccionado)
+        {
+            //se cumplio el tiempo de espera
+            //hacemos un nuevo objeto
+            GameObject copia = Instantiate(prefabSpawner, transform.position, transform.rotation);
+
+            //cada que aparezca un nuevo asteroide actualizamos el momento en el que lo hizo
+            ultimoMomentoSpawn = Time.time;
+
+            //decidimos cuanto esperar para cuanto crear una nueva copia
+            tiempoSeleccionado = Random.Range(tiempoMinimoSpawn, tiempoMaxSpawn);
+        }
+    }
+}
